2bdf88529bc87a853f921cf10684c67329d27df2
John Urban <mr.john.urban@gmail.com>
2016-11-24 12:10:44 -0500

    Added quivervars.awk.sh -- analogous output to pilonchanges.awk.sh

--- quivervars.awk.sh
+++ quivervars.awk.sh
@@ -14,4 +14,4 @@ function msg {
 if [ $# -eq 0 ]; then msg; exit; fi
 
 
-awk 'OFS="\t" {if ( $1 ~ /^#/ ) h+=1; else if($3 == "substitution") m+=1; else if($3 == "insertion") i+=1; else if($3 == "deletion") d+=1} END{print m,i,d,m+i+d,NR-h,m/(m+i+d),i/(m+i+d),d/(m+i+d)}' $f
+awk 'OFS="\t" {if ( $1 ~ /^#/ ) h+=1; else if($3 == "substitution") m+=1; else if($3 == "insertion") i+=1; else if($3 == "deletion") d+=1} END{print m,i,d,m+i+d,NR-h,m/(m+i+d),i/(m+i+d),d/(m+i+d)}' $1
