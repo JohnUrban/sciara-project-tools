ad53c079ca301e0144d683a31ed286a63b53cf05
John Urban <mr.john.urban@gmail.com>
2016-11-24 11:05:34 -0500

    added simple pilonchanges.awk.sh script.

--- pilonchanges.awk.sh
+++ pilonchanges.awk.sh
@@ -5,7 +5,9 @@ function msg {
 	pilonchanges.awk.sh pilon.changes
 
 	Output:
-		numMisMatch numInsertion numDeletion TotalCount TotalLinesSeen
+		numMisMatch numInsertion numDeletion TotalCount TotalLinesSeen propMisMatch propIns propDel
+
+	Uses TotalCount to get proportions. TotalLinesSeen is provided as a QC that TotalCount equals TotalLinesSeen.
 "
 }
 
@@ -13,4 +15,4 @@ if [ $# -eq 0 ]; then msg; exit; fi
 
 
 
-awk 'OFS="\t" {if($3 == "." && $4 != ".") i+=1; else if($3 != "." && $4 == ".") d+=1; else if($3 != "." && $4 != ".") m +=1}END{print m,i,d,m+i+d,NR}' $1
+awk 'OFS="\t" {if($3 == "." && $4 != ".") i+=1; else if($3 != "." && $4 == ".") d+=1; else if($3 != "." && $4 != ".") m +=1}END{print m,i,d,m+i+d,NR,m/(m+i+d),i/(m+i+d),d/(m+i+d)}' $1
