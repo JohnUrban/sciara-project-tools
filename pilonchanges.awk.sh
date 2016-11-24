4c273d04ffc6c62852a34b3fcca391350a2bebfd
John Urban <mr.john.urban@gmail.com>
2016-11-24 11:03:02 -0500

    added simple pilonchanges.awk.sh script.

--- pilonchanges.awk.sh
+++ pilonchanges.awk.sh
@@ -3,6 +3,9 @@
 function msg {
   echo "Usage:
 	pilonchanges.awk.sh pilon.changes
+
+	Output:
+		numMisMatch numInsertion numDeletion TotalCount TotalLinesSeen
 "
 }
 
@@ -10,4 +13,4 @@ if [ $# -eq 0 ]; then msg; exit; fi
 
 
 
-awk 'OFS="\t" {if($3 == "." && $4 != ".") i+=1; else if($3 != "." && $4 == ".") d+=1; else if($3 != "." && $4 != ".") m +=1}END{print m,i,d}' $1
+awk 'OFS="\t" {if($3 == "." && $4 != ".") i+=1; else if($3 != "." && $4 == ".") d+=1; else if($3 != "." && $4 != ".") m +=1}END{print m,i,d,m+i+d,NR}' $1
