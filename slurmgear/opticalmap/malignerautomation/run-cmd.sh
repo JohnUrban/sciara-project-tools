a54137d541019cc03d103071fc73737e31cf2507
John Urban <mr.john.urban@gmail.com>
2017-03-01 14:38:24 -0500

    updating run script for maligner n slurmgear

--- slurmgear/opticalmap/malignerautomation/run-cmd.sh
+++ slurmgear/opticalmap/malignerautomation/run-cmd.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
+
+# Make sure variables below work for you.
+# Make sure to have the ASMFOFN (usually input.fofn) in working dir.
+
 CLEAN=false
-CONFIG=/gpfs/data/sgerbi/jurban/software/maligner/scripts/maligner-config-sciara.cfg
+CONFIG=/users/jurban/software/sciaratools/sciara-project-tools/slurmgear/opticalmap/malignerautomation/maligner-config-sciara.cfg
 ASMFOFN=input.fofn
-MAPSFOFN=/gpfs/data/sgerbi/jurban/software/maligner/scripts/bionanomaps.fofn
+MAPSFOFN=/users/jurban/software/sciaratools/sciara-project-tools/slurmgear/opticalmap/malignerautomation/bionanomaps.examp2.fofn
 REC_ENZ=BssSI
 REC_SEQ=CACGAG
 
-##RUN=/gpfs/data/sgerbi/jurban/software/maligner/scripts/auto-malign.sh 
 RUN=/users/jurban/software/sciaratools/sciara-project-tools/slurmgear/opticalmap/malignerautomation/auto-malign.sh
+
+
+
+################ EXECUTE #####################
 
 $RUN $CLEAN $CONFIG $ASMFOFN $MAPSFOFN $REC_ENZ $REC_SEQ
 
+################ EXECUTE #####################
+
+
+##CONFIG=/gpfs/data/sgerbi/jurban/software/maligner/scripts/maligner-config-sciara.cfg
+##MAPSFOFN=/gpfs/data/sgerbi/jurban/software/maligner/scripts/bionanomaps.fofn
+##RUN=/gpfs/data/sgerbi/jurban/software/maligner/scripts/auto-malign.sh 
