120a72a0213264b3220bc077a095e313d662dc6a
John Urban <mr.john.urban@gmail.com>
2017-03-13 15:52:31 -0400

    added qualdist.py

---
+++ qualdist.py
@@ -0,0 +1,17 @@
+#!/bin/usr/env python
+from Bio import SeqIO
+from collections import defaultdict
+import sys
+## adapted from poretools qualdist
+
+
+qual_count = defaultdict(int)
+total_nucs = 0
+
+for fq in SeqIO.parse(sys.argv[1], "fastq"):
+    for q in fq.letter_annotations['phred_quality']:
+        qual_count[q] += 1
+        total_nucs += 1
+for q in qual_count:
+    print '\t'.join(str(s) for s in [chr(q+33), q, qual_count[q], 
+        total_nucs, float(qual_count[q]) / float(total_nucs)])
