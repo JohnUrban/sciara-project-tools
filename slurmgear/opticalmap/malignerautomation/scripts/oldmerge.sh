0f40e16a34614c1a1e8d6bcb900113f4572d353d
John Urban <mr.john.urban@gmail.com>
2017-04-27 18:06:25 -0400

    updated slurmgrad maligner merge script

--- slurmgear/opticalmap/malignerautomation/scripts/oldmerge.sh
+++ slurmgear/opticalmap/malignerautomation/scripts/oldmerge.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-F1=`ls ${ALN}/*aln | head -n 1`
-head -n 1 $F1 > all.bionano.smoothed.maps.aln
-
-for f in ${ALN}/*.smoothed.maps*aln; do
+head -n 1 ${ALN}/bionano.smoothed.maps00.aln > all.bionano.smoothed.maps.aln
+for f in ${ALN}/bionano.smoothed.maps*aln; do
     tail -n +2 $f >> all.bionano.smoothed.maps.aln
 done
 tail -n +2 all.bionano.smoothed.maps.aln | wc -l > num_alignments.txt
