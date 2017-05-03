c864c1a065d9344b6766d0532a015fd16833328f
John Urban <mr.john.urban@gmail.com>
2017-05-03 16:14:05 -0400

    update slurmgear - developing transeval

--- slurmgear/transcripteval/scripts/analysis.sh
+++ slurmgear/transcripteval/scripts/analysis.sh
@@ -8,6 +8,6 @@ for i in $(seq $NJOBS); do
   awk -v "i=$i" -v "n=$n" 'OFS="\t" {p+=$3*$4/100.0; b+=$10; l+=$4}END{printf "%d\t%f\t%f\t%f\t%f\n", i,p,b,l,n}' $f; 
 done > indiv.txt
 
-awk '{p+=$2; b+=$3; l+=$4, n+=$5}END{printf "%f\t%f\t%f\t%f\n", p, b, l, n}' indiv.txt > total.txt
+awk '{p+=$2; b+=$3; l+=$4; n+=$5}END{printf "%f\t%f\t%f\t%f\n", p, b, l, n}' indiv.txt > total.txt
 
 
