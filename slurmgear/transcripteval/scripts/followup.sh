ff3e10ac2929ab1792ff88beebcfe4c22c6d592d
John Urban <mr.john.urban@gmail.com>
2017-05-03 15:19:34 -0400

    update slurmgear - developing transeval

--- slurmgear/transcripteval/scripts/followup.sh
+++ slurmgear/transcripteval/scripts/followup.sh
@@ -50,9 +50,9 @@ if [ $NUM_TO_FIX -gt 0 ]; then
   #sbatch ...
   echo FIX MORE
   FOLLOWDONE=`sbatch --dependency=${BLASTDONE} -J ${BASE}_blast_trans_followup_${FOLLOWUPNUM} \
-     -o ${OUT}/follow_up_${FOLLOWUPNUM}.slurm.%A.out \
+     -o ${SLURMOUTDIR}/follow_up_${FOLLOWUPNUM}.slurm.%A.out \
      --mem=2g --time=6:00:00 -c 2 --qos=$QOS \
-     --export=BASE=${BASE},NJOBS=${NJOBS},SLURMOUTDIR=${OUT},SLURMPRE=blast_trans_followup_${FOLLOWUPNUM}.slurm,FOLLOWUPNUM=${FOLLOWUPNUM},BMEM=${BMEM},BTIME=${BTIME},BTHREADS=${BTHREADS},QOS=${QOS},SCRIPTS=${SCRIPTS},QUERYDIR=${QUERYDIR},PRE=${PRE},BLASTDIR=${BLASTDIR},BDB=${BDB},TASK=${TASK},EVAL=${EVAL},WORDSIZE=${WORDSIZE},CULL=${CULL},MAXTARGSEQ=${MAXTARGSEQ} \
+     --export=BASE=${BASE},NJOBS=${NJOBS},SLURMOUTDIR=${SLURMOUTDIR},SLURMPRE=blast_trans_followup_${FOLLOWUPNUM}.slurm,FOLLOWUPNUM=${FOLLOWUPNUM},BMEM=${BMEM},BTIME=${BTIME},BTHREADS=${BTHREADS},QOS=${QOS},SCRIPTS=${SCRIPTS},QUERYDIR=${QUERYDIR},PRE=${PRE},BLASTDIR=${BLASTDIR},BDB=${BDB},TASK=${TASK},EVAL=${EVAL},WORDSIZE=${WORDSIZE},CULL=${CULL},MAXTARGSEQ=${MAXTARGSEQ} \
      ${SCRIPTS}/followup.sh | awk '{print $4}'`
 elif [ $NUM_TO_FIX -eq 0 ]; then
   #sbatch
@@ -62,7 +62,7 @@ elif [ $NUM_TO_FIX -eq 0 ]; then
   cd $D
   ## there should not be any dependencies by definition at this point
   FOLLOWDONE=`sbatch -J ${BASE}_trans_blastout_analysis \
-     -o ${OUT}/analysis.slurm.%A.out \
+     -o ${SLURMOUTDIR}/analysis.slurm.%A.out \
      --mem=2g --time=6:00:00 -c 2 --qos=$QOS \
      --export=NJOBS=${NJOBS},BLASTDIR=${BLASTDIR} \
      ${SCRIPTS}/analysis.sh | awk '{print $4}'`
