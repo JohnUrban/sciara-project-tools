0a1f0f6c4b58da6273fbe5edb56448aee6156467
John Urban <mr.john.urban@gmail.com>
2017-05-03 16:07:26 -0400

    update slurmgear - developing transeval

--- slurmgear/transcripteval/scripts/analysis.sh
+++ slurmgear/transcripteval/scripts/analysis.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# matchlength, bitscore, length
+
 for i in $(seq $NJOBS); do 
   f=${BLASTDIR}/*.${i}.blastout; 
-  awk -v "i=$i" 'OFS="\t" {p+=$3*$4/100.0; b+=$10}END{print i,p,b}' $f; 
+  n=`awk '{print $1}' $f | sort | uniq | wc -l`
+  awk -v "i=$i" -v "n=$n" 'OFS="\t" {p+=$3*$4/100.0; b+=$10; l+=$4}END{printf "%d\t%f\t%f\t%f\t%f\n", i,p,b,l,n}' $f; 
 done > indiv.txt
 
-awk '{p+=$2; b+=$3}END{print p, b}' indiv.txt > total.txt
+awk '{p+=$2; b+=$3; l+=$4}END{printf "%f\t%f\t%f\t%f\n", p, b, l, n}' indiv.txt > total.txt
 
 
