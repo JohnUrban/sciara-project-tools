bf8c68da2de12c35bc51ba83069ac449b9961c5e
John Urban <mr.john.urban@gmail.com>
2017-05-02 21:01:30 -0400

    update slurmgear - developing transeval

--- slurmgear/transcripteval/scripts/transblast.sh
+++ slurmgear/transcripteval/scripts/transblast.sh
@@ -19,7 +19,7 @@ echo MAXTARGSEQ ${MAXTARGSEQ}
 
 
 CMD="blastn -task $TASK -db $BDB -query ${QUERYDIR}/${PRE}.${SLURM_ARRAY_TASK_ID}.fa 
- -evalue $1e-1 -word_size $WORDSIZE -culling_limit $CULL \
+ -evalue $EVAL -word_size $WORDSIZE -culling_limit $CULL \
  -max_target_seqs $MAXTARGSEQ -num_threads $P \
  -out ${BLASTDIR}/${PRE}.${SLURM_ARRAY_TASK_ID}.blastout \
  -outfmt '6 qseqid sseqid pident length qstart qend sstart send evalue bitscore qlen slen sstrand' "
