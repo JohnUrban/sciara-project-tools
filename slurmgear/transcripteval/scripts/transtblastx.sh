cdd9882a62cec70fbf11dad0c23ea54ad1c13c5f
John Urban <mr.john.urban@gmail.com>
2017-05-04 08:10:54 -0400

    update slurmgear -- adding tblastx analyses to transeval

--- slurmgear/transcripteval/scripts/transtblastx.sh
+++ slurmgear/transcripteval/scripts/transtblastx.sh
@@ -26,5 +26,11 @@ CMD="tblastx -strand plus -db $BDB -query ${QUERYDIR}/${PRE}.${SLURM_ARRAY_TASK_
 echo $CMD
 
 
+tblastx -strand plus -db $BDB -query ${QUERYDIR}/${PRE}.${SLURM_ARRAY_TASK_ID}.fa \
+ -evalue $EVAL -word_size $WORDSIZE -culling_limit $CULL \
+ -max_target_seqs $MAXTARGSEQ -num_threads $P \
+ -out ${BLASTDIR}/${PRE}.${SLURM_ARRAY_TASK_ID}.blastout \
+ -outfmt '6 qseqid sseqid pident length qstart qend sstart send evalue bitscore qlen slen sstrand'
+
 
 echo; date
