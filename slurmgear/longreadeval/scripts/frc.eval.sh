789ca21a4485355ebcd590aa354851626972eedc
John Urban <mr.john.urban@gmail.com>
2017-05-09 14:43:07 -0400

    update slurmgear

--- slurmgear/longreadeval/scripts/frc.eval.sh
+++ slurmgear/longreadeval/scripts/frc.eval.sh
@@ -3,7 +3,7 @@
 module load boost/1.55.0  ##for frc
 PATH=~/software/frcbam/FRC_align/bin/:/users/jurban/software/sciaratools/sciara-project-tools/:$PATH
 
-echo BAM ${PBBAML2PE}
+echo BAM ${BAM}
 echo L2PE_MAXINS ${L2PE_MAXINS}
 echo GSIZE ${GSIZE}
 echo BASE ${BASE}
