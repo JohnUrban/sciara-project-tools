6bbad28e45991ec7699a0682daa42c278f63104d
John Urban <mr.john.urban@gmail.com>
2017-05-11 17:46:10 -0400

    updated IUPAC-to-ACGT.py

--- IUPAC-to-ACGT.py
+++ IUPAC-to-ACGT.py
@@ -26,6 +26,14 @@ DESCRIPTION
     N = A or C or G or T
 
     N will be kept as N by default as these usually indicate gaps.
+
+    Anticipated letters are in "AaCcGgTtNnKkMmSsRrWwYyBbDdHhVv"
+    Unanticipated letters are any other letter/symbol encountered in sequence (rare).
+    An example would be "X".
+
+    Unanticipated letters will be randomly changed to A, C, G, T.
+    You can turn off this default and return the unanticipated letter using -K
+    You can add un-anticipated letters using --anticipate argument to provide replacement options other than ACGT (e.g. just A)
     
     """, formatter_class= argparse.RawTextHelpFormatter)
 parser.add_argument('--fasta', '-f', required=True,
@@ -44,6 +52,49 @@ Default is 0 to allow for correctly learning frequencies from sequences missing 
 parser.add_argument('--convertN', '-N', 
                    action='store_true', default=False,
                    help='''Convert Ns into ACGT. Default: False.''')
+parser.add_argument('--upper', '-U', 
+                   action='store_true', default=False,
+                   help='''Return output sequence entirely upper-case instead of as is. Default: False.''')
+parser.add_argument('--lower', '-L', 
+                   action='store_true', default=False,
+                   help='''Return output sequence entirely lower-case instead of as is. Default: False.''')
+parser.add_argument('--keep_unexpected', '-K', 
+                   action='store_true', default=False,
+                   help='''When unanticipated letter is encountered, keep it as is instead of converting it using the N rule. Default: False.''')
+
+parser.add_argument('--anticipate', '-A', 
+                   type=str, default=False,
+                   help='''To anticipate an otherwise un-anticipated letter, follow this example for X:
+-A X:ACGT.
+This will replace X with A, C, G, or T.
+This does not assume X and x are the same.
+To anticipate both (or more than 1 letter), do:
+-A X,x:ACGT
+That is comma-separate all that will be replace with same letters.
+TTo replace another leeter with a different rule, follow example:
+-A X,x:ACGT;Z:AC
+That is, keep things for same rule comma-separate and new rule starts after ;
+
+Note that providing an already-antcipated letter (as defined way above) will result in that
+letter-replacement rule being changed to what you define here.
+Be careful.
+
+Replacements need to be in ACGT -- all uppercase.
+
+Note - if a replacement letter is not part of sample frequency, then
+it will be given a count of 1 to avoid throwing an error.
+This may be small compared to other counts.
+Example:
+Input sequence has no A: CCGGTTX
+You try to replace X with A or C: -A X:AC
+Since A did not occur in input sequence and therefore is 0 in terms of frquency,
+1 is added to its frequency to avoid a ZeroDivision error.
+
+
+Can use --pseudocount to avoid any ACGT being 0.
+Can use --samplesize 0 so all are set to 1.
+Note however that both will globally affect all replacement choices.''')
+
 args = parser.parse_args()
 
 
@@ -111,15 +162,46 @@ translate['v'] = lambda : choice(['a','c','g'], p = probs('ACG',freq))
 translate['N'] = lambda : choice(['A','C','G', 'T'], p = probs('ACGT',freq))
 translate['n'] = lambda : choice(['a','c','g','t'], p = probs('ACGT',freq))
 
+if args.anticipate:
+    rulepairs = args.anticipate.split(";")
+    for rule in rulepairs:
+        keylist, replacements = rule.split(":")
+        keys = keylist.split(",")
+        replist = [e for e in replacements]
+        for ltr in replist:
+            try:
+                assert freq[ltr] > 0
+            except:
+                freq[ltr] = 1.0
+        for key in keys:
+            translate[key] = lambda : choice(replist, p = probs(replacements,freq))
 
-
+        
 for fa in SeqIO.parse(args.fasta, 'fasta'):
+    seq = str(fa.seq)
+##    if args.upper:
+##        seq = seq.upper()
+##    elif args.lower:
+##        seq = seq.lower()
     newseq = ''
-    for ltr in str(fa.seq):
+    for ltr in seq:
         if ltr not in safe_ltrs:
-            newseq += translate[ltr]()
+            try:
+                newltr = translate[ltr]()
+            except:
+                #default to treating unantipated letters as N
+                if not args.keep_unexpected:
+                    newltr = translate['N']()
+                else: #keep unexpexted
+                    newltr = ltr
+                
+            newseq += newltr
         else:
             newseq += ltr
+    if args.upper:
+        newseq = newseq.upper()
+    elif args.lower:
+        newseq = newseq.lower()
     print ">"+fa.description
     print newseq
         
