fffc7f4a2c0d063655e1d2161304ee7d828c7342
John Urban <mr.john.urban@gmail.com>
2017-05-25 16:48:43 -0400

    update slurmgear

--- slurmgear/opticalmap/malignerautomation/scripts/clean.sh
+++ slurmgear/opticalmap/malignerautomation/scripts/clean.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 
-rm -r aln/ asm_map/
 
 
 cd merge/
 
 RM=false
 F=allstats.txt
-if [ -f $F ]; then X=`cat $F | wc -l`; if [ $X -ge 9 ]; then RM=true; fi; fi
+if [ -f $F ]; then X=`awk '$2!=""' $F | wc -l`; if [ $X -ge 9 ]; then RM=true; fi; fi
 
-if $RM; then rm *aln *bedGraph *genome; fi
+if $RM; then 
+  rm *aln *bedGraph *genome; 
+  rm -r ../aln/ ../asm_map/
+fi
 
 cd ../
 
