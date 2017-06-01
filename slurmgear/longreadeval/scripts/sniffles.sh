2f6e0ac85aec1dd6c80d19b55ae98f08a0132eb4
John Urban <mr.john.urban@gmail.com>
2017-06-01 16:15:30 -0400

    update slurmgear

--- slurmgear/longreadeval/scripts/sniffles.sh
+++ slurmgear/longreadeval/scripts/sniffles.sh
@@ -12,8 +12,26 @@ sniffles -m $BAM -b $BEDPE.bedpe --min_support $MINSUPPORT
 
 ## SVs from Sniffles
 ## NUM
-grep -c -v ^# $$BEDPE.bedpe > ${OUTPRE}numsv
+grep -c -v ^# $$BEDPE.bedpe > numsv
 
 ## SUM PREDICTED LENGTHS
-grep -v ^# $BEDPE.bedpe | awk -v "s=0" '{s+=$NF}END{print s}' > ${OUTPRE}sumsv
+grep -v ^# $BEDPE.bedpe | awk -v "s=0" '{s+=$NF}END{print s}' > sumsv
 
+
+## TYPES
+grep -v ^# $BEDPE.bedpe | awk -v "s=0" '$11=="DEL" {s+=1}END{print s}' > numdel
+grep -v ^# $BEDPE.bedpe | awk -v "s=0" '$11=="DUP" {s+=1}END{print s}' > numdup
+grep -v ^# $BEDPE.bedpe | awk -v "s=0" '$11=="INS" {s+=1}END{print s}' > numins
+grep -v ^# $BEDPE.bedpe | awk -v "s=0" '$11=="INV" || $11=="DEL/INV" || $11=="INVDUP" || $11=="INV/INVDUP" {s+=1}END{print s}' > numinv
+grep -v ^# $BEDPE.bedpe | awk -v "s=0" '$11=="TRA" {s+=1}END{print s}' > numtra
+
+
+## SV TYPES I ENCOUNTERED
+#  13419 DEL
+#      1 DEL/INV
+#   1268 DUP
+#   1037 INS
+#    167 INV
+#     65 INVDUP
+#      5 INV/INVDUP
+#   9185 TRA
