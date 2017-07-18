8c7e9f48d45bc510ecb951d63fc711964a364312
John Urban <mr.john.urban@gmail.com>
2017-07-18 11:46:19 -0400

    updated abspath

--- abspath.py
+++ abspath.py
@@ -7,8 +7,9 @@ parser = argparse.ArgumentParser(description="""
     Takes in paths to files -- can even do wildcards -- returns abs paths for each.
     
     """, formatter_class= argparse.RawTextHelpFormatter)
-parser.add_argument('files', metavar='FILES', nargs='+',
-                               help='The input files - as many as you want.')
+parser.add_argument('files', metavar='FILES', nargs='+', help='The input files - as many as you want.')
+parser.add_argument('-b', '--basename', action='store_true', default=False, help='''Return basename instead.''')
+parser.add_argument('-s', '--split', action='store_true', default=False, help='''Return path to dir and basename separately/split.''')
 
 args = parser.parse_args()
 
@@ -19,4 +20,9 @@ if len(args.files) == 1 and (args.files[0] == '-' or args.files[0] == 'stdin'):
         args.files += fpath.split()
 
 for fpath in args.files:
-    print os.path.abspath(fpath)
+    if args.basename:
+        print os.path.basename(fpath)
+    elif args.split:
+        print ("\t").join( os.path.split(os.path.abspath(fpath)) )
+    else: #abspath
+        print os.path.abspath(fpath)
