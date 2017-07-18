f511aa83cac371de6d48fec93e8218c9c5483ebf
John Urban <mr.john.urban@gmail.com>
2017-07-17 22:13:39 -0400

    updated slurmgear

--- slurmgear/shortreadeval/scripts/map.eval.sh
+++ slurmgear/shortreadeval/scripts/map.eval.sh
@@ -8,7 +8,7 @@ PATH=~/software/frcbam/FRC_align/bin/:/users/jurban/software/sciaratools/sciara-
  fi
 
  #quality check
- count=`samtools quickcheck -v -v -v -v -v -v reads.bam | grep -c "EOF marker is absent"`
+ count=`samtools quickcheck -vvvvv reads.bam 2>&1 | grep -c "EOF marker is absent"`
  if [ $count -gt 0 ]; then
    rm reads.bam
    bowtie2 -p $MTHREADS -q --very-sensitive -N 1 -x $BT2 -1 $R1 -2 $R2 2> $BASE.mapreads.err | samtools sort --threads $MTHREADS -o reads.bam
