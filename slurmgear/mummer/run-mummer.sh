753c0976b1d39083cef42be1c58b8722c6f8c53f
John Urban <mr.john.urban@gmail.com>
2017-07-20 23:12:04 -0400

    update slurmgear mummer

--- slurmgear/mummer/run-mummer.sh
+++ slurmgear/mummer/run-mummer.sh
@@ -6,15 +6,18 @@ for var in READSDIR PRE SLURM_ARRAY_TASK_ID MUMMER minMatchLength PREFIX breakLe
 done
 
 echo NUCMER
+echo ${MUMMER}/nucmer -minmatch ${minMatchLength} -prefix nuc/${PREFIX}.${SLURM_ARRAY_TASK_ID} -breaklen ${breakLen} -mincluster ${mincluster} -maxgap ${maxgap} ${REF} ${QUERY}
 ${MUMMER}/nucmer -minmatch ${minMatchLength} -prefix nuc/${PREFIX}.${SLURM_ARRAY_TASK_ID} -breaklen ${breakLen} -mincluster ${mincluster} -maxgap ${maxgap} ${REF} ${QUERY}
 
 echo; echo; echo
 
 echo DELTA FILTER
+echo ${MUMMER}/delta-filter -i ${identity} -l ${minAlnLen} -q nuc/${PREFIX}.${SLURM_ARRAY_TASK_ID}.delta > delfil/${PREFIX}.${SLURM_ARRAY_TASK_ID}.filtered.delta
 ${MUMMER}/delta-filter -i ${identity} -l ${minAlnLen} -q nuc/${PREFIX}.${SLURM_ARRAY_TASK_ID}.delta > delfil/${PREFIX}.${SLURM_ARRAY_TASK_ID}.filtered.delta
 
 echo; echo; echo
 
 echo SHOW COORD
+echo ${MUMMER}/show-coords -c -H -T -d -l -q delfil/${PREFIX}.${SLURM_ARRAY_TASK_ID}.filtered.delta | awk 'OFS="\t" {print $14,$1-1,$2,$7,$15,$3-1,$4,$5,$6,$8,$9,$10,$11,$12,$13}' > coords/${PREFIX}.${SLURM_ARRAY_TASK_ID}.filtered.delta.bed
 ${MUMMER}/show-coords -c -H -T -d -l -q delfil/${PREFIX}.${SLURM_ARRAY_TASK_ID}.filtered.delta | awk 'OFS="\t" {print $14,$1-1,$2,$7,$15,$3-1,$4,$5,$6,$8,$9,$10,$11,$12,$13}' > coords/${PREFIX}.${SLURM_ARRAY_TASK_ID}.filtered.delta.bed
 
