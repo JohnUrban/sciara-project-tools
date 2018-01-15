6398f06e618998493892cee4661fe866cad58c90
John Urban <mr.john.urban@gmail.com>
2018-01-15 14:13:53 -0500

    added utils to pufferfish

---
+++ pufferfish/pufferfish/utils/plot_gc_exp_distributions.R
@@ -0,0 +1,57 @@
+
+args <- commandArgs(trailingOnly=TRUE)
+
+gcdist <- args[1] ## Only argument is GC distribution file -- 2 columns: %GC, comma-separated values seen for that %GC
+
+PSEUDOCOUNT <- 1
+
+if (length(args) == 2){
+  fname <- args2
+} else {
+  fnamevec <- unlist(strsplit(args[1], split='/'))
+  basename <- fnamevec[length(fnamevec)]
+  fname <- paste0(basename,".exp.pdf")
+}
+print(fname)
+
+data <- read.table(gcdist, col.names=c('gc','dist'), colClasses=c('numeric','character'), stringsAsFactors = FALSE)
+
+pdf(fname)
+
+for (gc in data$gc){
+  d <- as.numeric( unlist( strsplit(data$dist[data$gc == gc], split = ',') ) )
+  d <- d+PSEUDOCOUNT
+  n <- length(d)
+  mu <- mean(d)
+  med <- median(d)
+  std <- sd(d)
+  mad <- median(abs(d-med))
+  mn <- min(d)
+  mx <- max(d)
+  
+  rmu <- rexp(n = n, rate = 1/mu)
+  rmed <- rexp(n = n, rate = 1/med)
+
+  RNG <- range(d,rmu,rmed)
+  l.out <- round(min(c(500, n/10)))
+  if (l.out < 2) {l.out <- 2}
+  breaks <- seq(RNG[1]-mad, RNG[2]+mad, length.out=l.out)
+  print(c(gc, RNG[1], RNG[2], l.out))
+
+  hd <- hist(d, breaks=breaks, plot = FALSE)
+  hrmu <- hist(rmu, breaks=breaks, plot=FALSE)
+  hrmed <- hist(rmed, breaks=breaks, plot=FALSE)
+  
+  plot(hd$mids, hd$counts, type='l', main=paste0('GC = ', gc), lwd=3, col="grey")
+  lines(hrmu$mids, hrmu$counts, col='red', lty=3)
+  lines(hrmed$mids, hrmed$counts, col='blue', lty=3)
+  points(mu, 0, col='blue')
+  points(med, 0,col='red')
+}
+
+  
+dev.off()
+
+
+
+
