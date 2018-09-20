30a517222f026f3e783255dc2cb48708a3f7fd4d
John Urban <mr.john.urban@gmail.com>
2018-09-20 17:23:15 -0400

    fold-change.py now also allows exclusion of low count kmers and intersection of only kmers in both files.

--- fold-change.py
+++ fold-change.py
@@ -30,10 +30,31 @@ parser.add_argument('-B', '--fileB', type=str,
 
 parser.add_argument('-C', '--pseudocount', type=int, default=0,
                     help = '''.''')
+
+parser.add_argument('-EA', '--excludeA', type=int, default=0,
+                    help = '''Exclude kmers from a fileA with raw counts less than this. Default 0.
+                            This operation is independent of fileB.
+                            It is mainly to exclude error kmers from fileA.
+                            Also see --excludeB.
+                            To exclude kmers from both that only appear in 1, see --intersect.''')
+parser.add_argument('-EB', '--excludeB', type=int, default=0,
+                    help = '''Exclude kmers from a fileB with raw counts less than this. Default 0.
+                            This operation is independent of fileA.
+                            It is mainly to exclude error kmers from fileB.
+                            Also see --excludeA.
+                            To exclude kmers from both that only appear in 1, see --intersect.''')
+parser.add_argument('-I', '--intersect', action='store_true', default=False,
+                    help = '''The default behavior is to analyze the union of kmers sets between fileA and fileB.
+                            Thus any kmer that appears in one and not the other will get a 0-count (or pseudocount)
+                            for the file it was absent in.
+                            Kmers that apear in only 1 file may be errors or may not be of interest for another reason.
+                            This tells it to analyze only kmers that were in both files (after --excludeA/B options).''')
+
+
 args = parser.parse_args()
 
 
-def get(fh):
+def get(fh, mincount=0):
     d = defaultdict(int)
     stdin = False
     if fh in ('-','stdin') or fh.startswith('<('):
@@ -46,7 +67,9 @@ def get(fh):
         
     for line in f:
         line = line.strip().split()
-        d[line[0]] = int(line[1])
+        count = int(line[1])
+        if count >= mincount:
+            d[line[0]] = int(line[1])
     if not stdin:
         f.close()
     return d
@@ -79,12 +102,14 @@ def table(d1,d2,id1,id2):
 if __name__ == "__main__":
 
     ## read in A
-    A = get(args.fileA)
-    B = get(args.fileB)
+    A = get(args.fileA, mincount=args.excludeA)
+    B = get(args.fileB, mincount=args.excludeB)
 
     ## all kmers
-    allkmers = list(set(A.keys()+B.keys()))
-
+    if args.intersect:
+        allkmers = set(A.keys()).intersect(set(B.keys()))
+    else:
+        allkmers = list(set(A.keys()).union(set(B.keys())))
 
     ## ensure each kmer in both
     for kmer in allkmers:
