74b69e99b7b64cd63c2b298c22c53baba2920ea5
John Urban <mr.john.urban@gmail.com>
2018-09-20 11:24:16 -0400

    Added basic fold-change calculator.

---
+++ fold-change.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python2.7
+import os, sys
+import argparse
+from collections import defaultdict
+from math import log10, log
+import numpy as np
+
+
+
+
+#################################################
+## Argument Parser
+#################################################
+parser = argparse.ArgumentParser(description = """
+
+
+
+
+John Urban (2015, 2016, 2017, 2018)
+
+    """, formatter_class = argparse.RawTextHelpFormatter)
+
+
+
+parser.add_argument('-A', '--fileA', type=str, default=6,
+                    help = '''.''')
+
+parser.add_argument('-B', '--fileB', type=str, default=6,
+                    help = '''.''')
+
+args = parser.parse_args()
+
+
+def get(fh):
+    d = defaultdict(int)
+    with open(fh) as f:
+        for line in f:
+            line = line.strip().split()
+            d[line[0]] = int(line[1])
+    return d
+
+def median(d):
+    return np.median(d.values())
+
+def mad(d,med):
+    return np.median(np.absolute(np.array(d.values()) - med))
+    
+
+def med_norm(d, med):
+    newd = {}
+    med = float(med)
+    for kmer in d.keys():
+        newd[kmer] = d[kmer]/med
+    return newd
+
+def mad_norm(d, med, mad):
+    newd = {}
+    med = float(med)
+    for kmer in d.keys():
+        newd[kmer] = (d[kmer]-med)/mad
+    return newd
+
+def table(d1,d2):
+    for kmer in sorted(d1.keys()):
+        print kmer, d1[kmer], d2[kmer], d1[kmer]/d2[kmer], np.log2(d1[kmer]/d2[kmer]) 
+
+if __name__ == "__main__":
+
+    ## read in A
+    A = get(args.fileA)
+    B = get(args.fileB)
+
+    ## all kmers
+    allkmers = list(set(A.keys()+B.keys()))
+
+
+    ## ensure each kmer in both
+    for kmer in allkmers:
+        A[kmer]
+        B[kmer]
+
+    ## obtain medians
+    med_A = median(A)
+    med_B = median(B)
+
+    Anorm = med_norm(A, med_A)
+    Bnorm = med_norm(B, med_B)
+
+    ## print
+    table(Anorm, Bnorm)
