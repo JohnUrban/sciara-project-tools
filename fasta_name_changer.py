7938b4585d2c5df2aec13ed2b0da5a9352f7d634
John Urban <mr.john.urban@gmail.com>
2019-01-07 12:28:33 -0500

    Added ability to sort fasta entries before name changing to fasta_name_changer.py

--- fasta_name_changer.py
+++ fasta_name_changer.py
@@ -48,6 +48,11 @@ parser.add_argument('--number', '-n',
                    action='store_true', default=False,
                    help='''Add entry number (e.g. _1) to end of name. Often sed with --replace.''')
 parser.add_argument('--key', default=False, type=str, help=''' Provide filename to store new_name-to-old_name map file (3 columns: new name, old name, old description).''')
+parser.add_argument('--sort', default=False, action='store_true', help=''' Before renaming, sort sequences from shortest to longest. Also specify --revsort to do longest to shortest.''')
+parser.add_argument('--namesort', default=False, action='store_true', help=''' Ue with --sort to do sort names instead.''')
+parser.add_argument('--revsort', default=False, action='store_true', help=''' Ue with --sort to do longest to shortest or with --sort and --namesort to do reverse order of names.''')
+
+
 args = parser.parse_args()
 if args.keep:
     chunk = args.keep-1
@@ -57,10 +62,41 @@ if args.key:
     key = open(args.key,'w')
 ## SeqIO automatically takes ">name" from ">name other info" with entry.name
 ## entry.description gives whole name "name other info" -- removes ">" in front
+
+
+
+## Only load entire file into memory if you need to sort
+if args.sort:
+    seqlist = []
+    for entry in SeqIO.parse(args.fasta, "fasta"):
+        desc = str(entry.description)
+        name = str(entry.name)
+        seq = str(entry.seq)
+        seqlen = len(seq)
+        seqlist.append([desc, seq, seqlen, name])
+    
+
+    if args.sort:
+        if args.namesort:
+            sort_i = 0
+        else:
+            sort_i = 2
+        seqlist.sort(key = lambda x: x[sort_i], reverse = args.revsort) ## sort on length
+
+if args.sort:
+    SS=seqlist
+else:
+    SS=SeqIO.parse(args.fasta, "fasta")
+
+
 seq_n=0
-for entry in SeqIO.parse(args.fasta, "fasta"):
+for entry in SS:   
+#for entry in seqlist:
+#for entry in SeqIO.parse(args.fasta, "fasta"):
     seq_n += 1
-    name = entry.description 
+    #name = entry.description
+    #name = entry[0]
+    name = entry[0] if args.sort else entry.description
     if args.keep:
 ##        i = args.keep-1
         name = name.split()[chunk]
@@ -74,9 +110,11 @@ for entry in SeqIO.parse(args.fasta, "fasta"):
         name += "_"+str(seq_n)
     #name = ">"+name
     sys.stdout.write(">" + name + "\n")
-    sys.stdout.write(str(entry.seq) + "\n")
+    outseq = entry[1] if args.sort else str(entry.seq)
+    sys.stdout.write(outseq + "\n")
     if args.key:
-        key.write(name+'\t'+entry.name+'\t'+entry.description+'\n')
+        outkey = name+'\t'+entry[3]+'\t'+entry[0]+'\n' if args.sort else name+'\t'+entry.name+'\t'+entry.description+'\n'
+        key.write(outkey)
 
 if args.key:
     key.close()
