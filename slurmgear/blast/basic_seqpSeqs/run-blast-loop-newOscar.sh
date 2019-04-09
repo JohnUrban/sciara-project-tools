f1514f05cf18a1ac9888308a475cc36995feb183
John Urban <mr.john.urban@gmail.com>
2019-04-09 11:05:16 -0400

    Minor updates to blast scripts.

---
+++ slurmgear/blast/basic_seqpSeqs/run-blast-loop-newOscar.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+ASM=
+
+TIGDIR=contigs/
+BLASTDIR=blast
+SLURMOUT=slurmout
+P=8
+MEM=30g
+TIME=24:00:00
+ACCT=biomed-condo
+E=1e-10
+
+mkdir -p $BLASTDIR
+mkdir -p $SLURMOUT
+mkdir -p $TIGDIR
+
+## Separate assembly into its components - 1 per file here.
+multiFasta2manyFastas.py -f ${ASM} -d ${TIGDIR}  ## output is all *.fa
+
+## BLAST each component separately
+for fa in ${TIGDIR}/*fa; do
+ echo $fa
+ b=`basename $fa .fa`
+ sbatch -a ${ACCT} --mem=$MEM --time=$TIME -c $P -o ${SLURMOUT}/${b}.%A.out -J ${b}_blast_NT --export=Q=${fa},P=${P},BLASTDIR=${BLASTDIR},E=${E} blastn.sh
+done
