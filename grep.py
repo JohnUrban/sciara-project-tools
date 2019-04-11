d79ef66ece31b03fca2053f39dd463750f21443c
John Urban <mr.john.urban@gmail.com>
2019-04-11 11:41:38 -0400

    Expanded functionality of grep.py.

--- grep.py
+++ grep.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python2.7
-import sys, argparse
+import sys, argparse, re
 from collections import defaultdict
 
 parser = argparse.ArgumentParser(description="""
@@ -16,8 +16,10 @@ parser.add_argument('--patterns', '-p',
                    help='''File of patterns.''')
 
 parser.add_argument('--column', '-c',
-                   type=int, required=True,
-                   help='''Column to search. 1-based.''')
+                   type=int, default=0,
+                   help='''Column to search. 1-based. Default is to match the entire line.
+Given a column number, it will require a match to the column.
+Using --scan will look for it in entire line or column rather than requiring the line or column to be exact match.''')
 
 parser.add_argument('--patterns_column', '-C',
                    type=int, default=0,
@@ -27,6 +29,9 @@ parser.add_argument('--v', '-v',
                    action='store_true', default=False,
                    help='''Same as grep -v: return lines NOT matched.''')
 
+parser.add_argument('--scan', '-S',
+                   action='store_true', default=False,
+                   help='''Scan method: look for exact match in entire line or column rather than requiring the line or column to be exact match..''')
 parser.add_argument('--delim', '-s',
                    type=str, default='\t',
                    help='''Delimiter of input file. Default: tab.''')
@@ -47,6 +52,17 @@ with open(args.patterns) as f:
             line = line.strip().split()
             patterns[line[pcolumn]] = 1
 
+patterns_regex = re.compile('|'.join(patterns.keys()))
+# determine pattern in function
+if args.scan:
+    def pattern_in(x):
+        if len(re.findall(patterns_regex, x)) > 0:
+            return True
+        return False
+else:
+    def pattern_in(x): ## returns 1 if part of dict construction above, 0 otherwise
+        return patterns[searchString]
+
 column = args.column - 1
 stdin = args.file in ['stdin', '-'] or args.file[:1] == '<('
 if stdin:
@@ -54,11 +70,16 @@ if stdin:
 else:
     f = open(args.file)
 for line in f:
-    line = line.strip().split(args.delim)
-    if patterns[line[column]] and not args.v:
-        print (args.delim).join( line )
-    elif not patterns[line[column]] and args.v:
-        print (args.delim).join( line )
+    #line = line.strip().split(args.delim)
+    line = line.strip()
+    # Get string to search
+    searchString = line.strip().split(args.delim)[column] if column >= 0 else line
+    if not args.v and pattern_in(searchString):
+        #print (args.delim).join( line )
+        print line
+    elif args.v and not pattern_in(searchString):
+        #print (args.delim).join( line )
+        print line
         
 if not stdin:
     f.close()
