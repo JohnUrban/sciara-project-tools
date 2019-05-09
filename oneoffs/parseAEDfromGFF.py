46213a2b85f92c3fbd949bf9b91460cd43a8db51
John Urban <mr.john.urban@gmail.com>
2019-05-09 10:14:37 -0400

    AED parsing

--- oneoffs/parseAEDfromGFF.py
+++ oneoffs/parseAEDfromGFF.py
@@ -19,13 +19,14 @@ args = parser.parse_args()
 d = {}
 with open(args.gff) as f:
     for line in f:
-        line = line.strip().split('\t')
         if line:
-            if line[0][0] != '#' and line[2] == 'mRNA':
-                desc = line[8].split(';')
-                for e in desc:
-                    k,v = e.split('=')
-                    d[k.strip('_')] = v
-                out = [d['Parent'], d['ID'], d['AED'], d['eAED']]
-                print '\t'.join(out)
+            line = line.strip().split('\t')
+            if line[0][0] != '#' and len(line)>3:
+                if line[2] == 'mRNA':
+                    desc = line[8].split(';')
+                    for e in desc:
+                        k,v = e.split('=')
+                        d[k.strip('_')] = v
+                    out = [d['Parent'], d['ID'], d['AED'], d['eAED']]
+                    print '\t'.join(out)
 
