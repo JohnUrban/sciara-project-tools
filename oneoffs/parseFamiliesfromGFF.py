2e70bd7da70c731f729ed8e4ca34ff846457e30d
John Urban <mr.john.urban@gmail.com>
2019-05-23 14:17:37 -0400

    parseFamiliesfromGFF.py allows namechanging

--- oneoffs/parseFamiliesfromGFF.py
+++ oneoffs/parseFamiliesfromGFF.py
@@ -8,6 +8,9 @@ parser = argparse.ArgumentParser(description="""
 
 DESCRIPTION - parse out entries from list of parents.
 
+On most/all systems, this should give same results as:
+    grep -f parents.txt file.gff
+
     """, formatter_class= argparse.RawTextHelpFormatter)
 
 parser.add_argument('--gff', '-g', '-i', '-f',
@@ -17,6 +20,10 @@ parser.add_argument('--gff', '-g', '-i', '-f',
 parser.add_argument('--parents', '-p',
                    type= str, default=False, required=True,
                    help='''Path to parents.txt.''')
+
+parser.add_argument('--namechanger','-n',
+                    type= str, default='',
+                    help='''Add given string to the front of all ID, Name, and Parent.''')
 args = parser.parse_args()
 
 
@@ -33,19 +40,44 @@ def is_in_parents(x):
             return True
     return False
 
+##def namechanger(line, d, prefix):
+##    keys = d.keys()
+##    for key in ('ID','Name','Parent'):
+##        if key in keys:
+##            d[key] =
+##            
+##    key = k.strip('_')
+##                    if key in ('ID','Name','Parent'):
+##                        d[key] = args.namechanger + v
+##                    else:
+##                        d[key] = v
+
 with open(args.gff) as f:
     for line in f:
+        d = {}
         if line:
             line = line.strip().split('\t')
             if line[0][0] != '#' and len(line)>3:
                 desc = line[8].rstrip(';').split(';')
+                altdesc = []
                 for e in desc:
                     try:
                         k,v = e.split('=')
-                        d[k.strip('_')] = v
+                        join = '='
+                        #d[k.strip('_')] = v
                     except: #GO terms, etc sep by :
                         k,v = e.split(':')
-                        d[k.strip('_')] = v
+                        join = ':'
+                        #d[k.strip('_')] = v
+                    d[k.strip('_')] = v
+                    if k in ('ID','Name','Parent'):
+                        altdesc.append( k + join + args.namechanger + v )
+                    else:
+                        altdesc.append( e )
+                altdesc = ';'.join(altdesc)
                 if is_in_parents(d['ID']):
+                    if args.namechanger:
+                        line = line[:7] + [altdesc] + line[9:]
+                        #pass #line = namechanger(line, d, args.namechanger)
                     print '\t'.join(line)
 
