20180800a3da921e711ed6e59abc710aa2e959d8
John Urban <mr.john.urban@gmail.com>
2019-05-29 17:15:21 -0400

    annotation stats utility for maker and stringtie GFF analysis

--- oneoffs/annotationGeneStats.py
+++ oneoffs/annotationGeneStats.py
@@ -598,9 +598,11 @@ class GFF(object):
         return self.mRNA_per_gene
 
     def get_stats(self, x, p=[0,0.05, 0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95,1.0]):
-        q = np.quantile(x, p)
-        return [np.mean(x), np.std(x, ddof=1), np.min(x), np.max(x), 'Q:'] + [e for e in  q]
-    
+        try:
+            q = np.quantile(x, p)
+            return [np.mean(x), np.std(x, ddof=1), np.min(x), np.max(x), 'Q:'] + [e for e in  q]
+        except:
+            return None
     def get_exon_per_gene_stats(self):
         return self.get_stats(np.array(self.get_exons_per_gene()))
 
